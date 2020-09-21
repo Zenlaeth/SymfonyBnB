a1587e0f1d72d92a1ff152296f16e80808116a20
Zenlaeth <zenlalaeth@gmail.com>
2020-09-21 17:09:55 +0200

    video6

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Ad;
+use App\Form\AdType;
 use App\Repository\AdRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdController extends AbstractController
@@ -21,7 +25,41 @@ class AdController extends AbstractController
             'ads' => $ads
         ]);
     }
+        
+    /**
+     * Permet de créer une annonce
+     *
+     * @Route("/ads/new", name="ads_create")
+     * 
+     * @return Response
+     */
+    public function create(Request $request, EntityManagerInterface $manager){
+        $ad = new Ad();
 
+        $form = $this->createForm(AdType::class, $ad);
+
+        $form->handleRequest($request);
+
+        
+        if($form->isSubmitted() && $form->isValid()){
+            $manager->persist($ad);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "L'annonce <strong>{$ad->getTitle()}</strong> a bien été enregistrée !"
+            );
+
+            return $this->redirectToRoute('ads_show', [
+                'slug' => $ad->getSlug()
+            ]);
+        }
+
+        return $this->render('ad/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+        
     /**
      * Permet d'afficher une seule annonce
      *
@@ -29,10 +67,8 @@ class AdController extends AbstractController
      *
      * @return Response
      */
-    public function show($slug, AdRepository $repo){
-        // Je récupère l'annonce qui correspond au slug !
-        $ad = $repo->findOneBySlug($slug);
-
+    public function show($slug, Ad $ad)
+    {
         return $this->render('ad/show.html.twig',[
             'ad' => $ad
         ]);
